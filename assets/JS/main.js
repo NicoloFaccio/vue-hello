d245d2cc0e9dc4d9b5f5639eccf4fffd0699528d
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-25 15:08:15 +0100

    feat: created the title and put the sentence in the html

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,1 +1,10 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const{createApp} = Vue 
+createApp({
+    data(){
+        return {
+            sentence: "Hello world!"
+        }
+    }
+}).mount("#app")
